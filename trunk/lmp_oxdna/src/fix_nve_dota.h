fb0c7acf49e80c9414e7f37d3509f007e3d5162e
Oliver Henrich <oliver.henrich@strath.ac.uk>
2016-08-16 21:09:59 +0000

    Changed permissions
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/cgdna@49 ed60b790-0a32-40d1-be52-1a37240c2919

---
+++ trunk/lmp_oxdna/src/fix_nve_dota.h
@@ -0,0 +1,66 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(nve/dota,FixNVEDota)
+
+#else
+
+#ifndef LMP_FIX_NVE_DOTA_H
+#define LMP_FIX_NVE_DOTA_H
+
+#include "fix_nve.h"
+
+namespace LAMMPS_NS {
+
+class FixNVEDota : public FixNVE {
+ public:
+  FixNVEDota(class LAMMPS *, int, char **);
+  void init();
+  void initial_integrate(int);
+  void final_integrate();
+
+ private:
+  double dt,dthlf,dthlfm;
+  class AtomVecEllipsoid *avec;
+  inline void vec3_to_vec4(const double * q, const double * v3, double * v4)
+  {
+    v4[0] = -q[1]*v3[0] - q[2]*v3[1] - q[3]*v3[2];
+    v4[1] =  q[0]*v3[0] + q[3]*v3[1] - q[2]*v3[2];
+    v4[2] = -q[3]*v3[0] + q[0]*v3[1] + q[1]*v3[2];
+    v4[3] =  q[2]*v3[0] - q[1]*v3[1] + q[0]*v3[2];
+  }
+  inline void vec4_to_vec3(const double * q, const double * v4, double * v3)
+  {
+    v3[0] = -q[1]*v4[0] + q[0]*v4[1] - q[3]*v4[2] + q[2]*v4[3];
+    v3[1] = -q[2]*v4[0] + q[3]*v4[1] + q[0]*v4[2] - q[1]*v4[3];
+    v3[2] = -q[3]*v4[0] - q[2]*v4[1] + q[1]*v4[2] + q[0]*v4[3];
+  }
+};
+
+}
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: Compute nve/dota requires atom style ellipsoid
+
+Self-explanatory.
+
+E: Fix nve/dota requires extended particles
+
+This fix can only be used for particles with a shape setting.
+
+*/
